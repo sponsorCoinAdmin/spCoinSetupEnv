c4bea930b55d1a715fbd6a0fb3f4a79bf4a2c9f7
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-09-06 20:03:42 -0400

    WIP

---
+++ spCoinSetup.sh
@@ -0,0 +1,10 @@
+# NOTE: Requires git setup
+
+currDir=$PWD
+repo=spCoinHardHatTests
+#repo=spCoinUniSwapTests
+#repo=spCoinSimpleSwapTest
+git clone --recurse-submodules git@github.com:sponsorCoinAdmin/$repo.git
+cd ./$repo/setupEnv
+. ./spCoinEnvSetup.sh | tee -a ../logs/spCoinEnvSetup.log
+cdspc
